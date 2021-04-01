b618cc5b0cd1b9986c8766ea28dbe66a67e206fa
Andre Julius <noromoron@gmail.com>
2021-04-01 04:28:23 +0200

    Reduce cas effect

--- cas.js
+++ cas.js
@@ -3,7 +3,7 @@ const gm = require("gm").subClass({ imageMagick: true });
 async function gm_cas(image_data) {
     return new Promise((resolve, reject) => {
         gm(image_data)
-            .in("-liquid-rescale", "50%")
+            .in("-liquid-rescale", "75%")
             .toBuffer("png", (err, buffer) => {
                 if (err) return reject(err);
                 resolve(buffer);
